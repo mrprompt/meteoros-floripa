51eea1a44874a382383913c7ea31f8fc09ce2817
Thiago Paes <mrprompt@gmail.com>
2020-06-11 01:16:57 -0300

    fix path

--- .bin/convert.bat
+++ .bin/convert.bat
@@ -9,7 +9,7 @@ setlocal
         set "targetfile=%%~na.mp4"
 
         setlocal enabledelayedexpansion
-            c:\bramon\tools\ffmpeg -n -i "!sourcefile!" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p "!sourcepath!!targetfile!"
+            ffmpeg -n -i "!sourcefile!" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p "!sourcepath!!targetfile!"
         endlocal
     )
 endlocal
